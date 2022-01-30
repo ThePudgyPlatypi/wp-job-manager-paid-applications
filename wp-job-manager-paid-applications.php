5f0f8a25188c1df22c296fbea936cb8c571a973e
Chris Stehm <cstehm@chrisstehm.com>
2022-01-30 13:59:22 -0500

    Initial Commit

---
+++ wp-job-manager-paid-applications.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name: WP Job Manager - Paid Applications
+ * Plugin URI: https://chrisstehm.com
+ * Description: A plugin that will allow you to charge for each application to a job listing
+ * Version: 1.0.0
+ * Author: Chris Stehm
+ * Author URI: https://chrisstehm.com
+ *
+ * Copyright: 2021 Chris Stehm
+ * License: GNU General Public License v3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * @package wp-job-manager-paid-applications
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
